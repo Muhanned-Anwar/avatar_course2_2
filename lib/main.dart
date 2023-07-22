748ffed149ba2b8b57b21c938334dc4053547e20
Muhanned-Anwar <mohannedanwarflutter@gmail.com>
2023-07-22 17:05:32 +0300

    add centerTitle

--- lib/main.dart
+++ lib/main.dart
@@ -23,6 +23,7 @@ class LoginScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        centerTitle: true,
         backgroundColor: Colors.black54,
         title: const Text(
           'Login Screen',
