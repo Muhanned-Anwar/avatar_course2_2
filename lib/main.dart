364ace787038c14fcf7af44bfd7d1da4bfab48ea
Muhanned-Anwar <mohannedanwarflutter@gmail.com>
2023-07-22 15:59:53 +0300

    mainAxisAlignment in column

--- lib/main.dart
+++ lib/main.dart
@@ -12,33 +12,25 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Container( // width: double.infinity
-          color: Colors.black,
-          child: Column( // width: 0 >> max(100, 200, 80, 20) = 200
-                      // height: 0 >> sum(200, 300, 100, 400) = 400
-            children: [
-              Container( // width: 0
-                color: Colors.red,
-                width: 100,
-                height: 200,
-              ),
-              Container( // width: 0
-                color: Colors.green,
-                width: 200,
-                height: 300,
-              ),
-              Container( // width: 0
-                color: Colors.yellow,
-                width: 80,
-                height: 100,
-              ),
-              Container(
-                color: Colors.blue,
-                width: 20,
-                height: 300,
-              )
-            ],
-          ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Container( // width: 0
+              color: Colors.red,
+              width: 300,
+              height: 200,
+            ),
+            Container( // width: 0
+              color: Colors.green,
+              width: 300,
+              height: 200,
+            ),
+            Container( // width: 0
+              color: Colors.blue,
+              width: 300,
+              height: 200,
+            ),
+          ],
         )
       ),
     );
