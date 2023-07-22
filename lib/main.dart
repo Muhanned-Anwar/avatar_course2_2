135599574d2ed923c45ebdf6c530abe29bd76521
Muhanned-Anwar <mohannedanwarflutter@gmail.com>
2023-07-22 17:59:35 +0300

    PopupMenuButton

--- lib/main.dart
+++ lib/main.dart
@@ -13,24 +13,22 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: LoginScreen(),
       theme: ThemeData(
-        useMaterial3: true,
-        scaffoldBackgroundColor: Color(0xffe8e5e5),
-        appBarTheme: const AppBarTheme(
-          centerTitle: true,
-          backgroundColor: Colors.transparent,
-          elevation: 0,
-          titleTextStyle: TextStyle(
-            fontSize: 22,
-            color: Colors.black
-          ),
-        )
-      ),
+          useMaterial3: true,
+          scaffoldBackgroundColor: Color(0xffe8e5e5),
+          appBarTheme: const AppBarTheme(
+            centerTitle: true,
+            backgroundColor: Colors.transparent,
+            elevation: 0,
+            titleTextStyle: TextStyle(fontSize: 22, color: Colors.black),
+          )),
     );
   }
 }
 
 class LoginScreen extends StatelessWidget {
-  const LoginScreen({Key? key}) : super(key: key);
+   LoginScreen({Key? key}) : super(key: key);
+
+  String initPopUpValue = 'profile';
 
   @override
   Widget build(BuildContext context) {
@@ -44,6 +42,52 @@ class LoginScreen extends StatelessWidget {
         leading: const Icon(
           Icons.arrow_back_ios_new,
         ),
+        actions: [
+          IconButton(
+            onPressed: () {
+              print('Hello There');
+            },
+            icon: const Icon(
+              Icons.search,
+              size: 34,
+            ),
+          ),
+          PopupMenuButton(
+            iconSize: 34,
+            elevation: 10,
+            color: Colors.white70,
+            initialValue: initPopUpValue,
+            itemBuilder: (context) => const [
+              PopupMenuItem(
+                child: Text('Profile'),
+                value: 'profile',
+              ),
+              PopupMenuItem(
+                value: 'language',
+                child: Row(
+                  children: [
+                    Icon(Icons.person),
+                    Text('Language'),
+                  ],
+                ),
+              ),
+              PopupMenuItem(
+                value: 'about',
+                child: Text('About'),
+              ),
+            ],
+            onSelected: (value){
+              initPopUpValue = value;
+              print(value);
+            },
+            onCanceled: (){
+              print('Canceled Pop Up');
+            },
+            onOpened: (){
+              print('Open Pop Up');
+            },
+          ),
+        ],
       ),
     );
   }
